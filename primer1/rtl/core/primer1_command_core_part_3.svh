18c7725efd70b282b547578c2e99bfd28d951d55
Hoaf <134198196+hoafff@users.noreply.github.com>
2026-08-02 16:47:37 +0700

    fix(primer1): enforce self-test zeroize session and result rules

--- primer1/rtl/core/primer1_command_core_part_3.svh
+++ primer1/rtl/core/primer1_command_core_part_3.svh
@@ -1,43 +1,35 @@
-rsp[39:32]=active_session_id[31:24]; rsp[47:40]=active_session_id[23:16];
-rsp[55:48]=active_session_id[15:8]; rsp[63:56]=active_session_id[7:0];
-rsp[71:64]=last_error[15:8]; rsp[79:72]=last_error[7:0];
-rsp[87:80]=active_transaction_id[15:8]; rsp[95:88]=active_transaction_id[7:0];
-rsp[103:96]=diagnostic_summary[31:24]; rsp[111:104]=diagnostic_summary[23:16];
-rsp[119:112]=diagnostic_summary[15:8]; rsp[127:120]=diagnostic_summary[7:0];
-emit_response(request_command_i,request_txid_i,FLAG_RESPONSE,16'd16,rsp);
-end
-end
 CMD_RUN_SELF_TEST: begin
 if (request_payload_length_i != 4 || pbyte(request_payload_i,2)!=0 || pbyte(request_payload_i,3)!=0)
 emit_error(request_command_i,request_txid_i,ERR_BAD_LENGTH,0);
+else if ({pbyte(request_payload_i,0),pbyte(request_payload_i,1)}==16'd0 ||
+(({pbyte(request_payload_i,0),pbyte(request_payload_i,1)} & ~SELFTEST_SUPPORTED_MASK)!=16'd0))
+emit_error(request_command_i,request_txid_i,ERR_NOT_SUPPORTED,
+{pbyte(request_payload_i,0),pbyte(request_payload_i,1)});
 else if (session_state != SESSION_SELF_TEST_REQUIRED && session_state != SESSION_READY_NO_SESSION)
 emit_error(request_command_i,request_txid_i,ERR_BAD_STATE,0);
 else begin
+selftest_requested_mask <= {pbyte(request_payload_i,0),pbyte(request_payload_i,1)};
 begin_retained(request_txid_i,request_command_i,incoming_fingerprint);
 session_state <= SESSION_SELF_TEST_RUNNING;
 operation_state <= OP_EXECUTING;
+emit_empty_success(request_command_i,request_txid_i);
+if (({pbyte(request_payload_i,0),pbyte(request_payload_i,1)} & TEST_ASCON)!=0) begin
 ascon_key_input <= 0; ascon_nonce_input <= 0;
 ascon_ad_input <= 0; ascon_pt_input <= 0;
 ascon_start <= 1'b1;
-emit_empty_success(request_command_i,request_txid_i);
 core_state <= C_ST_ASCON_WAIT;
+end else begin
+poly_zeroize <= 1'b1;
+core_state <= C_ST_ZERO_WAIT;
+end
 end
 end
 CMD_GET_TXN_RESULT: begin
-if (request_payload_length_i != 2) emit_error(request_command_i,request_txid_i,ERR_BAD_LENGTH,0);
-else if (!retained_valid || {pbyte(request_payload_i,0),pbyte(request_payload_i,1)} != retained_txid)
-emit_error(request_command_i,request_txid_i,ERR_RESULT_NOT_READY,0);
-else begin
-rsp='0;
-rsp[7:0]=retained_txid[15:8]; rsp[15:8]=retained_txid[7:0];
-rsp[23:16]={5'h0,retained_state}; rsp[31:24]=retained_command;
-rsp[39:32]=retained_code[15:8]; rsp[47:40]=retained_code[7:0];
-rsp[55:48]=retained_data_length[15:8]; rsp[63:56]=retained_data_length[7:0];
-for (ci=0;ci<16;ci=ci+1)
-if (ci<retained_data_length) rsp[8*(10+ci)+:8]=retained_data[8*ci+:8];
-emit_response(request_command_i,request_txid_i,FLAG_RESPONSE,
-16'd10+retained_data_length,rsp);
-end
+if (request_payload_length_i != 2)
+emit_error(request_command_i,request_txid_i,ERR_BAD_LENGTH,0);
+else
+emit_txn_result_response(request_command_i,request_txid_i,
+{pbyte(request_payload_i,0),pbyte(request_payload_i,1)});
 end
 CMD_RETIRE_TXN_RESULT: begin
 if (request_payload_length_i != 2) emit_error(request_command_i,request_txid_i,ERR_BAD_LENGTH,0);
@@ -49,6 +41,8 @@ CMD_ZEROIZE: begin
 if (request_payload_length_i != 4 || pbyte(request_payload_i,1)!=0 ||
 pbyte(request_payload_i,2)!=0 || pbyte(request_payload_i,3)!=0)
 emit_error(request_command_i,request_txid_i,ERR_BAD_LENGTH,0);
+else if (pbyte(request_payload_i,0)!=ZEROIZE_ALL)
+emit_error(request_command_i,request_txid_i,ERR_NOT_SUPPORTED,pbyte(request_payload_i,0));
 else begin
 begin_retained(request_txid_i,request_command_i,incoming_fingerprint);
 zeroize_from_command <= 1'b1; zeroize_due_to_fault <= 1'b0;
@@ -100,7 +94,15 @@ emit_empty_success(request_command_i,request_txid_i);
 end
 end
 CMD_ABORT_SESSION: begin
-if (request_payload_length_i != 4) emit_error(request_command_i,request_txid_i,ERR_BAD_LENGTH,0);
+if (request_payload_length_i != 4)
+emit_error(request_command_i,request_txid_i,ERR_BAD_LENGTH,0);
+else if ({pbyte(request_payload_i,0),pbyte(request_payload_i,1),
+pbyte(request_payload_i,2),pbyte(request_payload_i,3)}!=32'd0 &&
+!(staged_valid && staged_session_id=={pbyte(request_payload_i,0),pbyte(request_payload_i,1),
+pbyte(request_payload_i,2),pbyte(request_payload_i,3)}) &&
+!(active_valid && active_session_id=={pbyte(request_payload_i,0),pbyte(request_payload_i,1),
+pbyte(request_payload_i,2),pbyte(request_payload_i,3)}))
+emit_error(request_command_i,request_txid_i,ERR_BAD_SESSION,0);
 else begin
 staged_key<=0;active_key<=0;staged_nonce_prefix<=0;active_nonce_prefix<=0;
 staged_session_id<=0;active_session_id<=0;staged_valid<=0;active_valid<=0;
@@ -114,6 +116,10 @@ if (request_payload_length_i!=4 || pbyte(request_payload_i,2)!=8 || pbyte(reques
 emit_error(request_command_i,request_txid_i,ERR_BAD_LENGTH,0);
 else if (operation_state==OP_EXECUTING)
 emit_error(request_command_i,request_txid_i,ERR_BUSY,0);
+else if (operation_state==OP_RESULT_READY)
+emit_error(request_command_i,request_txid_i,ERR_RESULT_PENDING,0);
+else if (operation_state!=OP_IDLE)
+emit_error(request_command_i,request_txid_i,ERR_BAD_STATE,0);
 else if ((pbyte(request_payload_i,0)==1 || pbyte(request_payload_i,0)==2) && pbyte(request_payload_i,1)!=1)
 emit_error(request_command_i,request_txid_i,ERR_BAD_STATE,0);
 else if (pbyte(request_payload_i,0)==3 && pbyte(request_payload_i,1)!=3)
